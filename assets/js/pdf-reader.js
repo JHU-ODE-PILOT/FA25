483bd06b75b567907f610e8332666c01780f1889
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-08-29 14:56:40 -0400

    update pdf reader rule to this site only

---
+++ assets/js/pdf-reader.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const pdfLinks = document.querySelectorAll('a[href$=".pdf"]');  // Select all PDF links
+
+  // Attach click event listener to each PDF link
+  pdfLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+      const pdfUrl = link.getAttribute('href'); // Open the PDF renderer in a new tab with the PDF URL as a query parameter
+      if (!/^https?:\/\//i.test(pdfUrl)) {
+        event.preventDefault(); // Prevent default link behavior
+        window.open('https://jhu-ode-pilot.github.io/FA25/assets/html/pdf-reader.html?pdf=' + encodeURIComponent(pdfUrl), '_blank');
+      }
+    });
+  });
+});
--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,18 +1,5 @@
 // Open PDF and different websites in a new page
 
-document.addEventListener("DOMContentLoaded", function () {
-  const pdfLinks = document.querySelectorAll('a[href$=".pdf"]');  // Select all PDF links
-
-  // Attach click event listener to each PDF link
-  pdfLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
-      event.preventDefault(); // Prevent default link behavior
-      const pdfUrl = link.getAttribute('href'); // Open the PDF renderer in a new tab with the PDF URL as a query parameter
-      window.open('https://jhu-ode-pilot.github.io/FA25/assets/html/pdf-reader.html?pdf=' + encodeURIComponent(pdfUrl), '_blank');
-    });
-  });
-});
-
 document.querySelectorAll('a').forEach(link => {
     if (link.closest('navbar')) return;
 
