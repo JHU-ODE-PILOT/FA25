1e84be2ebffad58d216c0c96afcb9fb22436c480
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-09-14 17:39:15 -0400

    added disable page functions

---
+++ assets/js/disable-page.js
@@ -0,0 +1,35 @@
+const date = Date();
+
+const examTime = [
+    {
+        start: Date("2025-09-16T08:00:00"),
+        end: Date("2025-09-16T09:00:00")
+    },
+    {
+        start: Date("2025-10-07T08:00:00"),
+        end: Date("2025-10-07T09:00:00")
+    },
+    {
+        start: Date("2025-11-18T08:00:00"),
+        end: Date("2025-11-18T09:00:00")
+    },
+    {
+        start: Date("2025-12-15T09:00:00"),
+        end: Date("2025-12-15T12:00:00")
+    },
+]
+
+examTime.forEach((block) => {
+    if (date.getTime() >= block.start.getTime() && date.getTime() <= block.end.getTime()) {
+        disablePage();
+    } 
+})
+
+const disablePage = () => {
+    const contents = document.getElementById("contents");
+    contents.innerHTML = `<div style="text-align: center"><h1>Assesement in progress.</h1><br><h2>The page will be enabled once the assessment is over.</h2><br><button onclick="location.reload();">Refresh</button></div>`
+    const botnav = document.getElementById("btm");
+    botnav.innerHTML = `Good luck on your exam`;
+    const topnav = document.getElementById("top");
+    topnav.innerHTML = `</span> &nbsp; PILOT Learning for <i>Differential Equations</i> &nbsp; &nbsp; Exam Mode`;
+};
